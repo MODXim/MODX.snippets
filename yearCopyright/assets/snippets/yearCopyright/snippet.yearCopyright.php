a1ef0c8a27502173ae7f574c4a5c4edb428e02b7
Agel_Nash <iam@agel-nash.ru>
2013-05-01 15:39:59 +0600

    Update snippet.yearCopyright.php
    Добавлен параметр $sep - разделитель между годами

--- yearCopyright/assets/snippets/yearCopyright/snippet.yearCopyright.php
+++ yearCopyright/assets/snippets/yearCopyright/snippet.yearCopyright.php
@@ -1,5 +1,8 @@
 <?php
 $dy = isset($dy) ? (int)$dy : '2013'; //$dy - год разработки
-if($dy===0)$dy=date('Y');
-if(date('Y')==$dy)return date('Y');else return $dy.' &mdash; '.date('Y');
+if($dy===0) $dy=date('Y');
+
+$sep = isset($sep) ? $sep : '&mdash;'; //Разделитель между годами
+
+return (date('Y')==$dy) ? date('Y') : $dy.' '.$sep.' '.date('Y');
 ?>
